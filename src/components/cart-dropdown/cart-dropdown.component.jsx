83119c9daa08f9f49f14752f0de1a5cf36381de1
Olga Garkusha <olga.garkusha.uk@gmail.com>
2022-09-23 08:06:35 +0200

    fixed dropdown apearance

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import Btn from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
-import { selectCartItems } from '../../store/cart/cart.selector';
+import { selectCartItems, selectIsCartOpen } from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action.js'
 
 import {CartDropdownContainer, 
 		EmptyMessage,
@@ -12,10 +13,14 @@ import {CartDropdownContainer,
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
+  const isCartOpen = useSelector(selectIsCartOpen);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const checkoutHandler = () => navigate('/ckeckout')
-  
+  const checkoutHandler = () => {
+    navigate('/ckeckout');
+    dispatch(setIsCartOpen(!isCartOpen));
+  }
   return (
     <CartDropdownContainer>
         <CartItemsContainer>
