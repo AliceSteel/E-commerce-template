31b693a21f2f6a80c73f5515ab9919209420157e
Olga Garkusha <olga.garkusha.uk@gmail.com>
2022-10-04 10:37:00 +0200

    deleted shop data sheet

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const CategoryContainer = styled.div`
     width: 90%;
-    max-width: 1200px;
+    max-width: 1400px;
     margin: 0 auto 50px;
 
     h2 {
@@ -13,9 +13,8 @@ export const CategoryContainer = styled.div`
 
 export const CategoryWrap = styled.div`
     width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 10px;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-gap: 10px;
 `;
 
